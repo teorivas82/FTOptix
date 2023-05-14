05e3da193320141e89a207575d811e1fe4a3fe2a
teorivas82 <73444511+teorivas82@users.noreply.github.com>
2023-05-14 17:30:47 -0400

    Add files via upload

---
+++ Test_Blank/ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -0,0 +1,32 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+namespace Test_Blank
+{
+    public static class ObjectTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("Test_Blank");
+        public static readonly NodeId Main_Window = new NodeId(namespaceIndex, new Guid("cbf8670f118d21699dc657c04719fcae"));
+        public static readonly NodeId Panel1 = new NodeId(namespaceIndex, new Guid("66ea827156c59d5b022e5e2a9ed73dfe"));
+        public static readonly NodeId Popup1 = new NodeId(namespaceIndex, new Guid("648622a653f2a12814e874b64c215a35"));
+        public static readonly NodeId SQL = new NodeId(namespaceIndex, new Guid("ca43c902ce02dcd78ffb54daff18445d"));
+        public static readonly NodeId Panel2 = new NodeId(namespaceIndex, new Guid("42787b7b374564320bfb9b61792a7de7"));
+        public static readonly NodeId ScaleLayout1 = new NodeId(namespaceIndex, new Guid("9621da52fc2fe51403aad438d9e5374e"));
+        public static readonly NodeId MQTT_Client = new NodeId(namespaceIndex, new Guid("0df26bcb35c1b1635c7f618eac07d4da"));
+        public static readonly NodeId Data_Controls = new NodeId(namespaceIndex, new Guid("5614c13777c1b81c12ad1994406c253b"));
+        public static readonly NodeId Alarms = new NodeId(namespaceIndex, new Guid("ee247b34c830d4c1d9100a2a6c0cac17"));
+        public static readonly NodeId Panel3 = new NodeId(namespaceIndex, new Guid("0f3c812afebfc1e5a2b396b15168e02c"));
+        public static readonly NodeId Reporting = new NodeId(namespaceIndex, new Guid("fe8415215f61e5b2b69386f1913ddd02"));
+        public static readonly NodeId Plot = new NodeId(namespaceIndex, new Guid("71bf2a50e1e116b64801fbdbb5b6c987"));
+        public static readonly NodeId BasePanel = new NodeId(namespaceIndex, new Guid("17b49944474e82212e88d7fa8b2295b5"));
+    }
+
+    public static class VariableTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("Test_Blank");
+    }
+}
---
+++ Test_Blank/ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,71 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "cbf8670f118d21699dc657c04719fcae")]
+public class Main_Window : FTOptix.UI.Window
+{
+}
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "66ea827156c59d5b022e5e2a9ed73dfe")]
+public class Panel1 : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "648622a653f2a12814e874b64c215a35")]
+public class Popup1 : FTOptix.UI.Popup
+{
+}
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "ca43c902ce02dcd78ffb54daff18445d")]
+public class SQL : FTOptix.UI.RowLayout
+{
+}
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "42787b7b374564320bfb9b61792a7de7")]
+public class Panel2 : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "9621da52fc2fe51403aad438d9e5374e")]
+public class ScaleLayout1 : FTOptix.UI.ScaleLayout
+{
+}
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "0df26bcb35c1b1635c7f618eac07d4da")]
+public class MQTT_Client : FTOptix.UI.ScaleLayout
+{
+}
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "5614c13777c1b81c12ad1994406c253b")]
+public class Data_Controls : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "ee247b34c830d4c1d9100a2a6c0cac17")]
+public class Alarms : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "0f3c812afebfc1e5a2b396b15168e02c")]
+public class Panel3 : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "fe8415215f61e5b2b69386f1913ddd02")]
+public class Reporting : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "71bf2a50e1e116b64801fbdbb5b6c987")]
+public class Plot : FTOptix.UI.ScaleLayout
+{
+}
+
+[MapType(NamespaceUri = "Test_Blank", Guid = "17b49944474e82212e88d7fa8b2295b5")]
+public class BasePanel : FTOptix.UI.Panel
+{
+}
